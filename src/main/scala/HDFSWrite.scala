dce6f8b602dfd64cc3f6f0bdaccc28a6af78750b
AleandroPresta <all96p@libero.it>
2023-09-14 10:57:47 +0200

    Cambiamento file HDFSWrite

--- src/main/scala/HDFSWrite.scala
+++ src/main/scala/HDFSWrite.scala
@@ -1,6 +1,10 @@
 package com.example
 
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable.ListBuffer
 
 case object HDFSWrite {
   def main(args: Array[String]): Unit = {
@@ -10,22 +14,52 @@ case object HDFSWrite {
       .setAppName("Write HDFS Example")
       .setMaster("local")
       .set("spark.hadoop.dfs.replication", "3")
-      .set("spark.hadoop.dfs.block.size", "1048576")
-      //.setMaster("spark://spark-master:7077")
-      //.set("spark.locality.wait", "900000")
-      //.set("spark.executors.cores", "2")
+      .set("spark.hadoop.dfs.block.size", "10048576")
+    //.setMaster("spark://spark-master:7077")
+    //.set("spark.locality.wait", "900000")
+    //.set("spark.executors.cores", "2")
     val sc = new SparkContext(conf)
 
     println("\nWriting\n")
 
-    val prefix = "hdfs://172.19.0.4:9000"
-
-    conf.set("fs.defaultFS", prefix)
-
     try {
-      val numbersRdd1 = sc.parallelize((1 to 10000000).toList)
-      numbersRdd1.saveAsTextFile(prefix + "/EdgeCloud/data2.txt")
-      println(s"RDD class: ${numbersRdd1.getClass}")
+      val list = ListBuffer[Int]()
+      val rand = new scala.util.Random()
+      for (_ <- 1 to 10000) {
+        val res0 = rand.nextInt()
+        list += res0
+      }
+      println("Data sample:")
+      println(list.take(5).mkString(", "))
+      val conf = new Configuration()
+
+      // Set the URI of the Hadoop cluster
+      val hdfs = "hdfs://172.28.1.2:8020"
+      val newReplicationFactor = 3
+      conf.set("fs.defaultFS", hdfs)
+      // Set the new replication factor in the configuration
+      conf.set("dfs.replication", newReplicationFactor.toString)
+
+
+      // Create a FileSystem object based on the configuration
+      val fs = FileSystem.get(conf)
+
+      val outputPath = hdfs + "/user/aleandro/edgeData2.txt"
+
+      // Create a path object for the output file
+      val outputPathObj = new Path(outputPath)
+
+      // Create an output stream for the output file
+      val outputStream = fs.create(outputPathObj)
+
+      println("Saving array")
+      // Iterate over the array and write each element to the output stream
+      for (element <- list) {
+        outputStream.writeBytes(element.toString)
+        outputStream.writeBytes("\n") // Add a new line after each element
+      }
+      // Close the output stream
+      outputStream.close()
     } catch {
       case ex: org.apache.hadoop.mapred.FileAlreadyExistsException =>
         println("File1 already exists!")
